9c2d99dec4678442f2995ddfb4376b44f52dbfc6
Marc Ariza <45202074+MarcArizaAlborni@users.noreply.github.com>
2021-11-27 17:11:28 +0100

    Update GunScript.cs

--- MultiplayerGame_GOTY2K21/Assets/Scripts/Player/Guns/GunScript.cs
+++ MultiplayerGame_GOTY2K21/Assets/Scripts/Player/Guns/GunScript.cs
@@ -6,6 +6,8 @@ public class GunScript : MonoBehaviour
 
     public float damage = 5f;
     public float range = 100f;
+    float internalTimer = 0.3f;
+    public const float rateofFire = 0.2f;
 
 
     public Camera ourCamera;
@@ -19,18 +21,19 @@ public class GunScript : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        internalTimer -= Time.deltaTime;
+
+
+
+        if (Input.GetMouseButton(0) && internalTimer <= 0.0f)
         {
-           
             Shooting();
         }
-
-
-        
     }
 
     void Shooting()
     {
+        internalTimer = rateofFire;
         muzzleFlash.Play();
         RaycastHit hit;
         if(Physics.Raycast(ourCamera.transform.position, ourCamera.transform.forward, out hit, range))
