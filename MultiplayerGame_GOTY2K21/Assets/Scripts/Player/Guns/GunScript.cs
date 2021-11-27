f3f628f490e9b29429f6bfdc33faf90250b53cf4
Marc Ariza <45202074+MarcArizaAlborni@users.noreply.github.com>
2021-11-27 17:26:01 +0100

    working in recoil

--- MultiplayerGame_GOTY2K21/Assets/Scripts/Player/Guns/GunScript.cs
+++ MultiplayerGame_GOTY2K21/Assets/Scripts/Player/Guns/GunScript.cs
@@ -7,16 +7,24 @@ public class GunScript : MonoBehaviour
     public float damage = 5f;
     public float range = 100f;
     float internalTimer = 0.3f;
-    public const float rateofFire = 0.2f;
 
+    public const float rateofFire = 0.2f;
 
     public Camera ourCamera;
 
     public ParticleSystem muzzleFlash;
 
+
+    public Vector3 addedRecoil;//Recoil
+    public Vector3 initialRot; //Recoil
+    
+
     private void Start()
     {
         Cursor.visible = false;
+        initialRot = gameObject.transform.localEulerAngles;
+
+        addedRecoil = new Vector3(-3f, 0f, 0f);
     }
     void Update()
     {
@@ -24,15 +32,33 @@ public class GunScript : MonoBehaviour
         internalTimer -= Time.deltaTime;
 
 
-
         if (Input.GetMouseButton(0) && internalTimer <= 0.0f)
         {
             Shooting();
         }
+        else if (internalTimer <= 0.0f)
+        {
+
+        }
+        else
+        {
+           // ResetRecoil();
+        }
+    }
+
+    void SetRecoil()
+    {
+        gameObject.transform.localEulerAngles += addedRecoil;
+    }
+
+    void ResetRecoil()
+    {
+        gameObject.transform.localEulerAngles = initialRot;
     }
 
     void Shooting()
     {
+        //SetRecoil();
         internalTimer = rateofFire;
         muzzleFlash.Play();
         RaycastHit hit;
