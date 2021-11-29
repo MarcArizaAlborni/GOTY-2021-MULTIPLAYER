76ace80ce772fabf1a07cf0343cadc8f1ec63e81
Marc Ariza <45202074+MarcArizaAlborni@users.noreply.github.com>
2021-11-29 11:11:29 +0100

    ChungerWafferAdded

---
+++ MultiplayerGame_GOTY2K21/Assets/Scripts/Player/Guns/ChungerWafferScript.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChungerWafferScript : MonoBehaviour
+{
+    [Header("Gun Performance Management")]
+
+    public float fireRate = 0.1f;
+    public int clipSize = 30;
+    public int reservedAmmoCapacity = 90;
+    public float gunDamage = 5f;
+    public float gunRange = 100f;
+
+    [Header("Ammo Management")]
+    private bool canShoot;
+    private int currentAmmoMag;
+    private int ammoInReserve;
+
+    [Header("Hit Management")]
+    public Camera ourCamera;
+
+    [Header("Aim Management")]
+
+    public Vector3 normalLocalPosition;
+    public Vector3 aimingLocalPosition;
+    public float aimSpeed = 10;
+
+    [Header("Mouse Settings")]
+    public float mouseSensitivity = 1;
+    private Vector2 currentRotation;
+    public float weaponSwayAmount = 10;
+
+    [Header("Recoil Management")]
+    public bool randomizeRecoil;
+    public Vector2 randomRecoilConstraints;
+
+    [Header("Visual Management")]
+    public ParticleSystem muzzleFlash;
+
+    private void Start()
+    {
+        currentAmmoMag = clipSize;
+        ammoInReserve = reservedAmmoCapacity;
+        canShoot = true;
+    }
+
+    private void Update()
+    {
+        SetAim();
+
+
+        if (Input.GetMouseButtonDown(0) && canShoot && currentAmmoMag > 0)
+        {
+            canShoot = false;
+            currentAmmoMag--;
+            StartCoroutine(ShootGun());
+        }
+        else if (Input.GetKeyDown(KeyCode.R) && currentAmmoMag < clipSize && ammoInReserve > 0)
+        {
+            int amountNeeded = clipSize - currentAmmoMag;
+
+            if (amountNeeded >= ammoInReserve)
+            {
+                currentAmmoMag += ammoInReserve;
+                ammoInReserve -= amountNeeded;
+            }
+            else
+            {
+                currentAmmoMag = clipSize;
+                ammoInReserve -= amountNeeded;
+            }
+
+        }
+    }
+
+    private void SetRotation()
+    {
+        Vector2 mouseAxis = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseAxis *= mouseSensitivity;
+        currentRotation += mouseAxis;
+
+        currentRotation.y = Mathf.Clamp(currentRotation.y, -90, 90);
+
+        transform.localPosition += (Vector3)mouseAxis * weaponSwayAmount / 1000;
+
+        transform.root.localRotation = Quaternion.AngleAxis(currentRotation.x, Vector3.up);
+        transform.parent.localRotation = Quaternion.AngleAxis(-currentRotation.y, Vector3.right);
+
+
+    }
+    private void SetAim()
+    {
+        Vector3 target = normalLocalPosition;
+        if (Input.GetMouseButton(1))
+            target = aimingLocalPosition;
+
+        Vector3 desiredPosition;
+        desiredPosition = Vector3.Lerp(transform.localPosition, target, Time.deltaTime * aimSpeed);
+
+
+        transform.localPosition = desiredPosition;
+    }
+
+    private void DetermineRecoil()
+    {
+        transform.localPosition -= Vector3.forward * 0.1f;
+
+        if (randomizeRecoil)
+        {
+            float xRecoil = Random.Range(-randomRecoilConstraints.x, randomRecoilConstraints.x);
+            float yRecoil = Random.Range(-randomRecoilConstraints.y, randomRecoilConstraints.y);
+
+            Vector2 recoil = new Vector2(xRecoil, yRecoil);
+            currentRotation += recoil;
+        }
+    }
+
+    private IEnumerator ShootGun()
+    {
+        DetermineRecoil();
+        muzzleFlash.Play();
+
+        CheckForHit();
+
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
+    }
+
+    void CheckForHit()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(ourCamera.transform.position, ourCamera.transform.forward, out hit, gunRange))
+        {
+            Debug.Log(hit.transform.name);
+
+            GetHit enemyReceiveDamage = hit.transform.GetComponent<GetHit>();
+
+
+            if (enemyReceiveDamage != null)
+            {
+                enemyReceiveDamage.ReceiveDamage(gunDamage);
+            }
+
+        }
+    }
+
+
+}
--- MultiplayerGame_GOTY2K21/Assets/Scripts/Player/Guns/GunSwapScript.cs
+++ MultiplayerGame_GOTY2K21/Assets/Scripts/Player/Guns/GunSwapScript.cs
@@ -8,9 +8,11 @@ public class GunSwapScript : MonoBehaviour
 
    public  RifleScript rifleScript;
    public  PistolScript pistolScript;
+    public ChungerWafferScript chungerScript;
 
    public GameObject rifle;
    public GameObject pistol;
+    public GameObject chunger;
 
 
 
@@ -18,21 +20,34 @@ public class GunSwapScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha2) && !Input.GetMouseButton(1))
         {
-            if (rifle.activeSelf)
-            {
+           
+            
                 rifle.SetActive(false);
                 pistol.SetActive(true);
-            }
+            chunger.SetActive(false);
+            
            
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha1) && !Input.GetMouseButton(1))
         {
-            if (pistol.activeSelf)
-            {
+            
+            
                 rifle.SetActive(true);
                 pistol.SetActive(false);
-            }
+               chunger.SetActive(false);
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && !Input.GetMouseButton(1))
+        {
+           
+            
+                rifle.SetActive(false);
+                pistol.SetActive(false);
+            chunger.SetActive(true);
+
 
         }
 
