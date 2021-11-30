53ebd7462ba48a88ac724663e6986ff2b32ae3d4
Marc Ariza <45202074+MarcArizaAlborni@users.noreply.github.com>
2021-11-30 13:44:15 +0100

    bullets

---
+++ MultiplayerGame_GOTY2K21/Assets/Scripts/Player/Guns/BulletManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+
+    public float lifeTime = 5.0f;
+
+
+    private void Start()
+    {
+        StartCoroutine(DestroyBulletTime());
+    }
+
+     IEnumerator DestroyBulletTime()
+     {
+        yield return new WaitForSeconds(lifeTime);
+
+        Destroy(gameObject);
+     }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
