8335d50e6512d17e229c06bd4288d66776ae2d19
Sebas Delgado <naitsabesdv@gmail.com>
2021-12-06 14:14:24 +0100

    Steering Seek implementatio
    Need some tweaks on values to fix animation with the movement.

--- MultiplayerGame_GOTY2K21/Assets/Scripts/Enemies/MovementEnemy.cs
+++ MultiplayerGame_GOTY2K21/Assets/Scripts/Enemies/MovementEnemy.cs
@@ -6,6 +6,23 @@ using UnityEngine.AI;
 
 public class MovementEnemy : MonoBehaviour
 {
+    [Header("Seek Parameters")]
+    public float stopDistance = 2.0f;
+    public float slowDistance = 4.0f;
+    public float maxTurnSpeed = 5.0f;
+    public float maxSpeed = 5.0f;
+    public float acceleration = 2.0f;
+    public float turnAcceleration = 2.0f;
+
+    //Private variables for using SteeringSeek
+    private float turnSpeed;
+    private float movSpeed;
+    private Quaternion rotation;
+    private Vector3 movement;
+    private Vector3 position;
+    private Quaternion rot;
+    private float timer = 0;
+
     public Transform target;
     private NavMeshAgent agent;
     private Animator currentAnimation;
@@ -15,20 +32,93 @@ public class MovementEnemy : MonoBehaviour
         agent = gameObject.GetComponent<NavMeshAgent>();
         currentAnimation = gameObject.GetComponent<Animator>();
 
-        agent.SetDestination(target.position);
-        //play move
-        currentAnimation.SetBool("IsRunning", true);
-        currentAnimation.SetBool("IsIdle", false);
+        position = gameObject.transform.position;
+
+        SteeringSeek(target.position);
+        SetRunning();
     }
     // Update is called once per frame
     void Update()
     {
-        if (agent.transform.position == target.position)
+        SteeringSeek(target.position);
+        Debug.Log("Agent Position: " + agent.transform.position);
+        Debug.Log("Position: " + position);
+    }
+
+    void SteeringSeek(Vector3 targetPos)
+    {
+        if (Vector3.Distance(targetPos, position) < stopDistance)
         {
-            currentAnimation.SetBool("IsIdle", true);  //play idle
-            currentAnimation.SetBool("IsRunning", false);
+            SetAttack();
+            return;
+        }
+            
+
+            
+
+        //seek delay so it does not iterate every frame
+        if (timer > 0.5)
+        {
+            Seek(targetPos);
         }
 
 
-    }  
+        turnSpeed += turnAcceleration * Time.deltaTime;
+        turnSpeed = Mathf.Min(turnSpeed, maxTurnSpeed);
+
+        if (Vector3.Distance(targetPos, position) < slowDistance)
+        {
+            //slows movement when arriving destination
+            movSpeed = (maxSpeed * Vector3.Distance(targetPos, position)) / slowDistance;
+        }
+        else
+        {
+            movSpeed += acceleration * Time.deltaTime;
+            movSpeed = Mathf.Min(movSpeed, maxSpeed);
+        }
+
+        rot = Quaternion.Slerp(rot, rotation, Time.deltaTime * turnSpeed);
+        position += transform.forward.normalized * movSpeed * Time.deltaTime;
+
+        if (timer > 0.5)
+        {
+            agent.destination = position;
+            timer = 0;
+        }
+        transform.rotation = rot;
+
+        timer += Time.smoothDeltaTime;
+    }
+
+    void Seek(Vector3 targetPos)
+    {
+        Vector3 direction = targetPos - position;
+        direction.y = 0f;
+        movement = direction.normalized * acceleration;
+        float angle = Mathf.Rad2Deg * Mathf.Atan2(movement.x, movement.z);
+        rotation = Quaternion.AngleAxis(angle, Vector3.up);
+    }
+
+
+    //Animation functions
+    void SetAttack()
+    {
+        currentAnimation.SetBool("IsRunning", false);
+        currentAnimation.SetBool("IsDead", false);
+        currentAnimation.SetBool("IsAttacking", true);
+    }
+
+    void SetDead()
+    {
+        currentAnimation.SetBool("IsRunning", false);
+        currentAnimation.SetBool("IsDead", true);
+        currentAnimation.SetBool("IsAttacking", false);
+    }
+
+    void SetRunning()
+    {
+        currentAnimation.SetBool("IsRunning", true);
+        currentAnimation.SetBool("IsDead", false);
+        currentAnimation.SetBool("IsAttacking", false);
+    }
 }
