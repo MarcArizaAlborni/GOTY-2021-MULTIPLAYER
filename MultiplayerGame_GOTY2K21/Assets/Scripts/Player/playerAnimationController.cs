0e171ac018cb7c63060265e22d09379325df9257
M1R4B3L <amirabelmartinez@gmail.com>
2021-12-12 21:29:00 +0100

    running animation maybe i need to redo everything :)

--- MultiplayerGame_GOTY2K21/Assets/Scripts/Player/playerAnimationController.cs
+++ MultiplayerGame_GOTY2K21/Assets/Scripts/Player/playerAnimationController.cs
@@ -6,28 +6,43 @@ public class playerAnimationController : MonoBehaviour
 {
     Animator animator;
     int isWalkingHash;
+    int isRunningHash;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool goForward = Input.GetKey("w");
+        bool fwrdPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
         bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+  
 
-        if (!isWalking && goForward)
+        if (!isWalking && fwrdPressed)
         {
             animator.SetBool(isWalkingHash, true);
         }
 
-        if (isWalking && !goForward)
+        if (isWalking && !fwrdPressed)
         {
             animator.SetBool(isWalkingHash, false);
+        }
+
+        if (!isRunning && (fwrdPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!fwrdPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
         }
     }
 }
