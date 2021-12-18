9a9bc04f0b05f411cefc89dc0cc0f7fd5bdbb786
M1R4B3L <amirabelmartinez@gmail.com>
2021-12-18 22:38:36 +0100

    Animations full done

--- MultiplayerGame_GOTY2K21/Assets/Scripts/Networking/TestingInput.cs
+++ MultiplayerGame_GOTY2K21/Assets/Scripts/Networking/TestingInput.cs
@@ -65,7 +65,7 @@ public class TestingInput : MonoBehaviour
         //Debug.Log(mouseX + ", " + mouseY);
     
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
         cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
     }
--- MultiplayerGame_GOTY2K21/Assets/Scripts/Player/playerAnimationController.cs
+++ MultiplayerGame_GOTY2K21/Assets/Scripts/Player/playerAnimationController.cs
@@ -8,8 +8,14 @@ public class playerAnimationController : MonoBehaviour
     TestingInput input;
     int isWalkingHash;
     int isRunningHash;
+    int isShootingHash;
+    int currentGunHash;
 
     Transform parentTransfrom;
+
+    public Camera ourCamera;
+    public float WalkingFov = 50.0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,38 +25,71 @@ public class playerAnimationController : MonoBehaviour
         input = GetComponent<TestingInput>();
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
+        isShootingHash = Animator.StringToHash("isShooting");
+        currentGunHash = Animator.StringToHash("currentGun");
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*bool fwrdPressed = Input.GetKey("w");
+        bool fwrdPressed = Input.GetKey("w");
         bool bwrdPressed = Input.GetKey("s");
-        bool runPressed = Input.GetKey("left shift");
+        /*bool runPressed = Input.GetKey("left shift");
         bool isWalking = animator.GetBool(isWalkingHash);
         bool isRunning = animator.GetBool(isRunningHash);*/
-   
+        bool leftClickPressed = Input.GetMouseButton(0);
+        bool rightClickPressed = Input.GetMouseButton(1);
+
+        bool onePressed = Input.GetKey("1");
+        bool twoPressed = Input.GetKey("2");
+
+        bool isShooting = animator.GetBool(isShootingHash);
+
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
 
         if(movement.magnitude > 0)
         {
             movement.Normalize();
             movement *= input.characterSpeed * Time.deltaTime;
-
         }
 
+        float rotX = ourCamera.transform.eulerAngles.x;
         float velZ = Vector3.Dot(movement.normalized, parentTransfrom.forward);
         float velX = Vector3.Dot(movement.normalized, parentTransfrom.right);
 
-      
         animator.SetFloat("VelZ", -velZ, 0.1f, Time.deltaTime);
         animator.SetFloat("VelX", velX, 0.1f, Time.deltaTime);
-    
 
 
+        if (rotX >= 91.0f)
+        {
+            rotX = rotX - 360.0f;
+            animator.SetFloat("aimVert", rotX, 0.1f, Time.deltaTime);
+        }
+        else
+        {
+            animator.SetFloat("aimVert", rotX, 0.1f, Time.deltaTime);
+        }
+
+        if(onePressed)
+        {
+            animator.SetInteger(currentGunHash, 0);
+        }
+        if(twoPressed)
+        {
+            animator.SetInteger(currentGunHash, 1);
+        }
 
 
+        /*if(!isShooting && leftClickPressed)
+        {
+            animator.SetBool(isShootingHash, true);
+        }
 
+        if (isShooting && !leftClickPressed)
+        {
+            animator.SetBool(isShootingHash, false);
+        }*/
 
         /*if (!isWalking && fwrdPressed)
         {
